891d64b41f1c68d8901fa623eb84ff326d9f9ab8
Ascendina <camilaascendina@gmail.com>
2019-09-08 17:57:24 -0300

    Programas iniciais

---
+++ EstruturaRepeticao.py
@@ -0,0 +1,11 @@
+#for com letras
+for c in "python":
+    print(c)
+
+#Em intervalo de valores
+for i in range(6):
+    print(i)
+
+#Em intervalo definido inicio e fim
+for a in range(0,5):
+    print(a)
---
+++ PrimeiroPrograma.py
@@ -0,0 +1,42 @@
+## Aqui eh o print
+print("Olá Mundo!")
+
+##aqui eh uma variavel
+'''num = int(input("Digite um número: \n"))
+num1 = int(input("Digite um outro número: \n"))
+
+print("soma: " + str(num + num1))
+print("subtração: " + str(num - num1))
+print("Multiplicação: " + str(num * num1))
+print("Divisão: " + str(num / num1))
+'''
+
+#Verificador de idade
+'''Pessoas com mais de 60 anos - idosas
+Pessoas com mais 25 anos - adultos
+Pessoas entre 12 e 25 - adolescente
+pessoas menores 12 - crianças'''
+
+'''idade = int(input("Digite a sua idade: \n"))
+
+if(idade >= 60):
+    print("Você é um idoso! Seja bem vindo a melhor idade!")
+elif(idade >= 25 and idade < 60):
+    print("Você é um adulto!")
+elif(idade >= 12 and idade < 25):
+    print("Você é um aborrecente!")
+else:
+    print("Você é uma criança!")
+'''
+
+'''Entrar com um número e apresentar se ele 
+é divisível por 5 ou não'''
+
+num = int(input("Digite um número: \n"))
+
+if(num % 5 == 0):
+    print("Divisível por 5!")
+else:
+    print("Não é divisível por 5")
+
+
