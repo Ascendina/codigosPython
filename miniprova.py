dcc33b1e800930f88719da1222b4903770fa1cc8
Ascendina <camilaascendina@gmail.com>
2019-09-17 13:30:16 -0300

    Revisão para a Mini Prova

---
+++ miniprova.py
@@ -0,0 +1,35 @@
+#entrada
+letra = input("Digite aqui: ")
+numero = int(input("Digite aqui"))
+nFracao = float(input("Digite aqui"))
+
+#Saida
+print("texto")
+print(numero)
+print("Texto" + str(numero))
+
+#Estrutura de escolha
+if(numero > 0):
+    #aqui ta o codigo
+elif(numero <= 10):
+    #aqui ta codigo
+else:
+    #aqui ta o codigo
+
+#Estrutura de repeticao
+#fora do for
+#for i in range(inicio, fim, incrementar)
+for i in range(10,0,-1):
+    #aqui fica o codigo
+#fora do for
+
+#fora
+while(numero != 10):
+    #aqui dentro
+    #numero = int(input)
+
+#fora
+
+#Variavel como memória
+contador = contador + 1
+mult = mult * 2
