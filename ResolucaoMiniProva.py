90ffeb74054fee537062c65d5ad8ac2590ebc53e
Ascendina <camilaascendina@gmail.com>
2019-09-17 13:29:48 -0300

    Resolução da Mini Prova

---
+++ ResolucaoMiniProva.py
@@ -0,0 +1,24 @@
+'''#entrada
+idade = int(input("Digite uma idade: "))
+
+if(idade >= 0 and idade <= 9):
+    print("Criança")
+elif(idade >= 10 and idade <= 19):
+    print("Adolescente")
+elif(idade >= 20 and idade <= 24):
+    print ("Jovem Adulto")
+elif(idade >= 25 and idade <= 59):
+    print("Adulto")
+elif(idade >= 60):
+    print("Idoso")
+else:
+    print("Idade inválida")
+'''
+
+n = 0
+while(n < 51):
+    print(n)
+    n = n + 1
+
+
+
